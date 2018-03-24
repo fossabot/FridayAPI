ebba0cb615d9617555a2978e4435b09a4519f948
ahmtcn123 <ahmetcanco@gmail.com>
2018-03-24 19:15:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var apiai = require('apiai')(process.env.API_token);
-
-var api = '253176417557282816';
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;
-
-var router = express.Router();    
-
-router.use(function(req, res, next) {
-    console.log('Kullanıcı bağlandı.');
-    next();
-});
+var request = require('request');
+var fridayapi = 'https://fridaymain.herokuapp.com'
+var fridayapikey = 'Some api key'
 
 
-app.get('/user/:userid/msg/:msgid', function (req, res) {
-	if(!api.includes(req.params.userid)) {
-		return res.send('Bu arayüzü kullanamazsınız. Sistemde kayıtlı değilsiniz geliştiriciye danışın.')
-		} 
-let apiaiReq = apiai.textRequest(`${req.params.msgid}`, {
-      sessionId: `${req.params.userid}`
-    });
-	
-apiaiReq.on('response', (response) => {
-      let aiText = response.result.fulfillment.speech;
-res.send(`${aiText}`)
+request(`${fridayapi}/user/${fridayapikey}/msg/(message)}`, function (error, response, body) {
+console.log(body)
 })
-apiaiReq.end();
-})
-
-app.get('/api', function (req, res) {
-	res.send(`FridayAI API v1 BETA`)
-})
-
-
-app.listen(port);
-console.log('Sunucu bağlandı. Sunucu portu:' + port);
