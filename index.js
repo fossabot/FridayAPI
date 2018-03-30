313d6b590b08f0fa5d74c68b16f3d9496b99adfa
ahmtcn123 <ahmetcanco@gmail.com>
2018-03-31 02:59:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var request = require('request');
-var fridayapi = 'https://fridaymain.herokuapp.com'
-var fridayapikey = 'Some api key'
-
-
-request(`${fridayapi}/user/${fridayapikey}/msg/(message)}`, function (error, response, body) {
-console.log(body)
-})
+Too soon
