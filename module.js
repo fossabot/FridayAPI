2945a043db204b6531534c9480f483b522bb54d0
ahmtcn123 <ahmetcanco@gmail.com>
2018-04-07 21:33:32 +0300

    Update module.js

--- module.js
+++ module.js
@@ -1,1 +1,30 @@
+var request = require('request');
+
+exports.ai = async (contentmsg,api, ai) => {
+if (contentmsg === '') {
+    console.log (Error('Bir mesaj algılanmadı'));
+  return;
+  };
+if (api === '') {
+    console.log (Error('Bir API KEY Girişi yapılmadı'));
+  return;
+  };
+if (api === 'test') {
+    console.log ('TEST OK');
+  return;
+  };
+  
+request(`https://fridaymain.herokuapp.com/user/${api}/msg/${contentmsg}`, function (error, response, body) {
+ai(body)
+})
+}
+
+//Ahmetcan Aksu 2018 
+  //FridayAI <3
+  
+
+
+
+
+
 
